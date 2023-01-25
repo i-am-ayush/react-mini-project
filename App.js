fa58ff9e445883ac8415e48633c3cf2fbb24c47f
i-am-ayush <46191140+i-am-ayush@users.noreply.github.com>
2023-01-25 23:29:30 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,27 +1,96 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const reactjsxheading1 = (
-  <h1 id="title" key="h2"> greatt </h1>
+import { RestaurantList } from "./constant";
+
+/**
+ * Header
+ *  logo
+ *  list items
+ *  cart
+ * body
+ *  searchbar
+ *  restaurantlist
+ *    restaurantcard
+ *      image
+ *      name
+ *      rating
+ *      cuisines
+ * footer
+ *  links
+ *  copyight
+ */
+
+//Jsx elements can have only one parent
+//if we dont want div then we can use reactfragment-- we cannot pass any style or anything
+const Title = () => (
+  <a href="/">
+    <img className="logo" alt="logo" src="https://cdn.vox-cdn.com/thumbor/5d_RtADj8ncnVqh-afV3mU-XQv0=/0x0:1600x1067/1200x900/filters:focal(672x406:928x662)/cdn.vox-cdn.com/uploads/chorus_image/image/57698831/51951042270_78ea1e8590_h.7.jpg"></img>
+  </a>
 )
 
-const Reactjsxheading1func = () => (
-  <h1 id="title" key="h2"> greatt2 </h1>
-)
-// const data = api.data()
-//react component
-//functional compo starts with capital name-not mandatory
-//we give (parenthesis) if jsx is in multiple lines
-//component composition - passing component inside component
-const HeaderComponent = () => {
+const Header = () => {
   return (
-    <div>
-      {/* {data}  we can write any piece of js code inside {} and jsx keeps sanitizing every malware attack, it keeps the app safe*/}
-      {reactjsxheading1}
-      {Reactjsxheading1func()}
-      <Reactjsxheading1func/>
-      <h1>functional component</h1><h2>functional component part 2</h2>
+    <div className="header">
+      <Title />
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
     </div>
   )
 }
+// config driven ui
+// const mehtaFood = {
+//   name: "mehta food",
+//   image: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/cadd4qgaodthe0ulvbgv",
+//   cuisine: ["indian", "chinese"],
+//   rating: "4.3"
+// }
+
+const RestaurantCard = ({
+  name,
+  cuisines,
+  cloudinaryImageId,
+  lastMileTravelString,
+}) => {
+  return (
+    <div className="card">
+      <img src= {`https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/${cloudinaryImageId}`}/>
+      <h2>{name}</h2>
+      <h3>{cuisines.join(", ")}</h3>
+      <h4>{lastMileTravelString} minutes</h4>
+    </div>
+  )
+}
+
+const Body = () => {
+  return (
+    <div className="restaurantCard">
+      {
+        RestaurantList.map(restaurant =>{
+          return <RestaurantCard {...restaurant.data}/>
+        })
+      }
+    </div>
+  )
+}
+
+const Footer = () => {
+  return <h4>footer</h4>
+}
+
+const AppLayout = () => {
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  )
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeaderComponent/>);
+root.render(<AppLayout />);
---
+++ constant.js
@@ -0,0 +1,569 @@
+export const RestaurantList = [
+    {
+      "type": "restaurant",
+      "data": {
+        "type": "F",
+        "id": "641565",
+        "name": "Somen19",
+        "uuid": "2074882e-f0bd-4b4e-a97f-0326f2af6bd9",
+        "city": "79",
+        "area": "Srikrishnapuri",
+        "totalRatingsString": "Too Few Ratings",
+        "cloudinaryImageId": "ed517d693c01179f5a25d5320ce9fcab",
+        "cuisines": [
+          "Indian",
+          "Biryani",
+          "Desserts"
+        ],
+        "tags": [],
+        "costForTwo": 50000,
+        "costForTwoString": "₹500 FOR TWO",
+        "deliveryTime": 32,
+        "minDeliveryTime": 32,
+        "maxDeliveryTime": 32,
+        "slaString": "32 MINS",
+        "lastMileTravel": 2.799999952316284,
+        "slugs": {
+          "restaurant": "somen19-srikrishnapuri-srikrishnapuri",
+          "city": "patna"
+        },
+        "cityState": "79",
+        "address": "23-B, Srikrishnapuri, West Boring Canal Road, Patna, Bihar _800001",
+        "locality": "West Boring Canal Road",
+        "parentId": 387336,
+        "unserviceable": false,
+        "veg": false,
+        "select": false,
+        "favorite": false,
+        "tradeCampaignHeaders": [],
+        "aggregatedDiscountInfo": {
+          "header": "20% off",
+          "shortDescriptionList": [
+            {
+              "meta": "20% off | Use JUMBO",
+              "discountType": "Percentage",
+              "operationType": "RESTAURANT"
+            }
+          ],
+          "descriptionList": [
+            {
+              "meta": "20% off up to ₹120 on orders above ₹450 | Use code JUMBO",
+              "discountType": "Percentage",
+              "operationType": "RESTAURANT"
+            }
+          ],
+          "subHeader": "",
+          "headerType": 0,
+          "superFreedel": ""
+        },
+        "aggregatedDiscountInfoV2": {
+          "header": "20% OFF",
+          "shortDescriptionList": [
+            {
+              "meta": "Use JUMBO",
+              "discountType": "Percentage",
+              "operationType": "RESTAURANT"
+            }
+          ],
+          "descriptionList": [
+            {
+              "meta": "20% off up to ₹120 on orders above ₹450 | Use code JUMBO",
+              "discountType": "Percentage",
+              "operationType": "RESTAURANT"
+            }
+          ],
+          "subHeader": "",
+          "headerType": 0,
+          "superFreedel": ""
+        },
+        "ribbon": [
+          {
+            "type": "PROMOTED"
+          }
+        ],
+        "chain": [],
+        "feeDetails": {
+          "fees": [
+            {
+              "name": "distance",
+              "fee": 1900,
+              "message": ""
+            },
+            {
+              "name": "time",
+              "fee": 0,
+              "message": ""
+            },
+            {
+              "name": "special",
+              "fee": 0,
+              "message": ""
+            }
+          ],
+          "totalFees": 1900,
+          "message": "",
+          "title": "Delivery Charge",
+          "amount": "1900",
+          "icon": ""
+        },
+        "availability": {
+          "opened": true,
+          "nextOpenMessage": "",
+          "nextCloseMessage": ""
+        },
+        "longDistanceEnabled": 0,
+        "rainMode": "NONE",
+        "thirdPartyAddress": false,
+        "thirdPartyVendor": "",
+        "adTrackingID": "cid=5758931~p=1~eid=00000185-e9e1-dcc5-0d0c-a5b300990129",
+        "badges": {
+          "imageBased": [],
+          "textBased": [],
+          "textExtendedBadges": []
+        },
+        "lastMileTravelString": "2.7 kms",
+        "hasSurge": false,
+        "sla": {
+          "restaurantId": "641565",
+          "deliveryTime": 32,
+          "minDeliveryTime": 32,
+          "maxDeliveryTime": 32,
+          "lastMileTravel": 2.799999952316284,
+          "lastMileDistance": 0,
+          "serviceability": "SERVICEABLE",
+          "rainMode": "NONE",
+          "longDistance": "NOT_LONG_DISTANCE",
+          "preferentialService": false,
+          "iconType": "EMPTY"
+        },
+        "promoted": true,
+        "avgRating": "--",
+        "totalRatings": 0,
+        "new": false
+      },
+      "subtype": "basic"
+    },
+    {
+      "type": "restaurant",
+      "data": {
+        "type": "F",
+        "id": "88781",
+        "name": "Sujata Family Restaurant",
+        "uuid": "fd40ca5b-21a3-4c9d-a47b-7f0d1c6715ff",
+        "city": "79",
+        "area": "Fraser Road Area",
+        "totalRatingsString": "10000+ ratings",
+        "cloudinaryImageId": "np13tsnfrxylkrk1ykm5",
+        "cuisines": [
+          "Mughlai",
+          "Chinese",
+          "North Indian"
+        ],
+        "tags": [],
+        "costForTwo": 30000,
+        "costForTwoString": "₹300 FOR TWO",
+        "deliveryTime": 35,
+        "minDeliveryTime": 35,
+        "maxDeliveryTime": 35,
+        "slaString": "35 MINS",
+        "lastMileTravel": 0.8999999761581421,
+        "slugs": {
+          "restaurant": "sujata-family-restaurant-fraser-road-fraser-road",
+          "city": "patna"
+        },
+        "cityState": "79",
+        "address": "Hotel Mayur, Fraser Road Area, Patna",
+        "locality": "Frazer road",
+        "parentId": 196672,
+        "unserviceable": false,
+        "veg": false,
+        "select": false,
+        "favorite": false,
+        "tradeCampaignHeaders": [],
+        "aggregatedDiscountInfo": {
+          "header": "60% off",
+          "shortDescriptionList": [
+            {
+              "meta": "60% off | Use TRYNEW",
+              "discountType": "Percentage",
+              "operationType": "RESTAURANT"
+            }
+          ],
+          "descriptionList": [
+            {
+              "meta": "60% off up to ₹120 | Use code TRYNEW",
+              "discountType": "Percentage",
+              "operationType": "RESTAURANT"
+            }
+          ],
+          "subHeader": "",
+          "headerType": 0,
+          "superFreedel": ""
+        },
+        "aggregatedDiscountInfoV2": {
+          "header": "60% OFF",
+          "shortDescriptionList": [
+            {
+              "meta": "Use TRYNEW",
+              "discountType": "Percentage",
+              "operationType": "RESTAURANT"
+            }
+          ],
+          "descriptionList": [
+            {
+              "meta": "60% off up to ₹120 | Use code TRYNEW",
+              "discountType": "Percentage",
+              "operationType": "RESTAURANT"
+            }
+          ],
+          "subHeader": "",
+          "headerType": 0,
+          "superFreedel": ""
+        },
+        "chain": [],
+        "feeDetails": {
+          "fees": [
+            {
+              "name": "distance",
+              "fee": 1900,
+              "message": ""
+            },
+            {
+              "name": "time",
+              "fee": 0,
+              "message": ""
+            },
+            {
+              "name": "special",
+              "fee": 0,
+              "message": ""
+            }
+          ],
+          "totalFees": 1900,
+          "message": "",
+          "title": "Delivery Charge",
+          "amount": "1900",
+          "icon": ""
+        },
+        "availability": {
+          "opened": true,
+          "nextOpenMessage": "",
+          "nextCloseMessage": ""
+        },
+        "longDistanceEnabled": 0,
+        "rainMode": "NONE",
+        "thirdPartyAddress": false,
+        "thirdPartyVendor": "",
+        "adTrackingID": "",
+        "badges": {
+          "imageBased": [],
+          "textBased": [],
+          "textExtendedBadges": []
+        },
+        "lastMileTravelString": "0.8 kms",
+        "hasSurge": false,
+        "sla": {
+          "restaurantId": "88781",
+          "deliveryTime": 35,
+          "minDeliveryTime": 35,
+          "maxDeliveryTime": 35,
+          "lastMileTravel": 0.8999999761581421,
+          "lastMileDistance": 0,
+          "serviceability": "SERVICEABLE",
+          "rainMode": "NONE",
+          "longDistance": "NOT_LONG_DISTANCE",
+          "preferentialService": false,
+          "iconType": "EMPTY"
+        },
+        "promoted": false,
+        "avgRating": "3.5",
+        "totalRatings": 10000,
+        "new": false
+      },
+      "subtype": "basic"
+    },
+    {
+      "type": "restaurant",
+      "data": {
+        "type": "F",
+        "id": "150950",
+        "name": "New Pal Hotel",
+        "uuid": "2dd6afab-7342-423e-a6c5-1b62fb8793b1",
+        "city": "79",
+        "area": "Fraser Road Area",
+        "totalRatingsString": "5000+ ratings",
+        "cloudinaryImageId": "cadd4qgaodthe0ulvbgv",
+        "cuisines": [
+          "Indian",
+          "Chinese",
+          "Tandoor"
+        ],
+        "tags": [],
+        "costForTwo": 20000,
+        "costForTwoString": "₹200 FOR TWO",
+        "deliveryTime": 27,
+        "minDeliveryTime": 27,
+        "maxDeliveryTime": 27,
+        "slaString": "27 MINS",
+        "lastMileTravel": 0.20000000298023224,
+        "slugs": {
+          "restaurant": "new-pal-hotel-station-road-fraser-road",
+          "city": "patna"
+        },
+        "cityState": "79",
+        "address": "Near Hotel City Center, opposite Laxmi Hotel, Station Road , Patna",
+        "locality": "Station road",
+        "parentId": 147430,
+        "unserviceable": false,
+        "veg": false,
+        "select": false,
+        "favorite": false,
+        "tradeCampaignHeaders": [],
+        "aggregatedDiscountInfo": {
+          "header": "60% off",
+          "shortDescriptionList": [
+            {
+              "meta": "60% off | Use TRYNEW",
+              "discountType": "Percentage",
+              "operationType": "RESTAURANT"
+            }
+          ],
+          "descriptionList": [
+            {
+              "meta": "60% off up to ₹120 | Use code TRYNEW",
+              "discountType": "Percentage",
+              "operationType": "RESTAURANT"
+            }
+          ],
+          "subHeader": "",
+          "headerType": 0,
+          "superFreedel": ""
+        },
+        "aggregatedDiscountInfoV2": {
+          "header": "60% OFF",
+          "shortDescriptionList": [
+            {
+              "meta": "Use TRYNEW",
+              "discountType": "Percentage",
+              "operationType": "RESTAURANT"
+            }
+          ],
+          "descriptionList": [
+            {
+              "meta": "60% off up to ₹120 | Use code TRYNEW",
+              "discountType": "Percentage",
+              "operationType": "RESTAURANT"
+            }
+          ],
+          "subHeader": "",
+          "headerType": 0,
+          "superFreedel": ""
+        },
+        "chain": [],
+        "feeDetails": {
+          "fees": [
+            {
+              "name": "distance",
+              "fee": 1900,
+              "message": ""
+            },
+            {
+              "name": "time",
+              "fee": 0,
+              "message": ""
+            },
+            {
+              "name": "special",
+              "fee": 0,
+              "message": ""
+            }
+          ],
+          "totalFees": 1900,
+          "message": "",
+          "title": "Delivery Charge",
+          "amount": "1900",
+          "icon": ""
+        },
+        "availability": {
+          "opened": true,
+          "nextOpenMessage": "",
+          "nextCloseMessage": ""
+        },
+        "longDistanceEnabled": 0,
+        "rainMode": "NONE",
+        "thirdPartyAddress": false,
+        "thirdPartyVendor": "",
+        "adTrackingID": "",
+        "badges": {
+          "imageBased": [],
+          "textBased": [],
+          "textExtendedBadges": []
+        },
+        "lastMileTravelString": "0.2 kms",
+        "hasSurge": false,
+        "sla": {
+          "restaurantId": "150950",
+          "deliveryTime": 27,
+          "minDeliveryTime": 27,
+          "maxDeliveryTime": 27,
+          "lastMileTravel": 0.20000000298023224,
+          "lastMileDistance": 0,
+          "serviceability": "SERVICEABLE",
+          "rainMode": "NONE",
+          "longDistance": "NOT_LONG_DISTANCE",
+          "preferentialService": false,
+          "iconType": "EMPTY"
+        },
+        "promoted": false,
+        "avgRating": "3.2",
+        "totalRatings": 5000,
+        "new": false
+      },
+      "subtype": "basic"
+    },
+    {
+      "type": "restaurant",
+      "data": {
+        "type": "F",
+        "id": "230250",
+        "name": "Pour Tous",
+        "uuid": "f1543804-a1e2-4244-b416-036633d7d56b",
+        "city": "79",
+        "area": "Patliputra Colony",
+        "totalRatingsString": "10000+ ratings",
+        "cloudinaryImageId": "lnjmdcml4anmh5hhdwv0",
+        "cuisines": [
+          "Biryani",
+          "Chinese",
+          "Kebabs",
+          "North Indian"
+        ],
+        "tags": [],
+        "costForTwo": 25000,
+        "costForTwoString": "₹250 FOR TWO",
+        "deliveryTime": 36,
+        "minDeliveryTime": 36,
+        "maxDeliveryTime": 36,
+        "slaString": "36 MINS",
+        "lastMileTravel": 5,
+        "slugs": {
+          "restaurant": "pour-toss-pataliputra-colony-pataliputra-colony",
+          "city": "patna"
+        },
+        "cityState": "79",
+        "address": "H.No- Behind Laddoo Gopal Sweets, Patliputra",
+        "locality": "Patliputra",
+        "parentId": 160184,
+        "unserviceable": false,
+        "veg": false,
+        "select": false,
+        "favorite": false,
+        "tradeCampaignHeaders": [],
+        "aggregatedDiscountInfo": {
+          "header": "10% off",
+          "shortDescriptionList": [
+            {
+              "meta": "10% off | Use TRYNEW",
+              "discountType": "Percentage",
+              "operationType": "RESTAURANT"
+            }
+          ],
+          "descriptionList": [
+            {
+              "meta": "10% off up to ₹40 | Use code TRYNEW",
+              "discountType": "Percentage",
+              "operationType": "RESTAURANT"
+            }
+          ],
+          "subHeader": "",
+          "headerType": 0,
+          "superFreedel": ""
+        },
+        "aggregatedDiscountInfoV2": {
+          "header": "10% OFF",
+          "shortDescriptionList": [
+            {
+              "meta": "Use TRYNEW",
+              "discountType": "Percentage",
+              "operationType": "RESTAURANT"
+            }
+          ],
+          "descriptionList": [
+            {
+              "meta": "10% off up to ₹40 | Use code TRYNEW",
+              "discountType": "Percentage",
+              "operationType": "RESTAURANT"
+            }
+          ],
+          "subHeader": "",
+          "headerType": 0,
+          "superFreedel": ""
+        },
+        "ribbon": [
+          {
+            "type": "PROMOTED"
+          }
+        ],
+        "chain": [],
+        "feeDetails": {
+          "fees": [
+            {
+              "name": "distance",
+              "fee": 2900,
+              "message": ""
+            },
+            {
+              "name": "time",
+              "fee": 0,
+              "message": ""
+            },
+            {
+              "name": "special",
+              "fee": 0,
+              "message": ""
+            }
+          ],
+          "totalFees": 2900,
+          "message": "",
+          "title": "Delivery Charge",
+          "amount": "2900",
+          "icon": ""
+        },
+        "availability": {
+          "opened": true,
+          "nextOpenMessage": "",
+          "nextCloseMessage": ""
+        },
+        "longDistanceEnabled": 0,
+        "rainMode": "NONE",
+        "thirdPartyAddress": false,
+        "thirdPartyVendor": "",
+        "adTrackingID": "cid=5782867~p=4~eid=00000185-e9e1-dcc5-0d0c-a5b400990475",
+        "badges": {
+          "imageBased": [],
+          "textBased": [],
+          "textExtendedBadges": []
+        },
+        "lastMileTravelString": "5 kms",
+        "hasSurge": false,
+        "sla": {
+          "restaurantId": "230250",
+          "deliveryTime": 36,
+          "minDeliveryTime": 36,
+          "maxDeliveryTime": 36,
+          "lastMileTravel": 5,
+          "lastMileDistance": 0,
+          "serviceability": "SERVICEABLE",
+          "rainMode": "NONE",
+          "longDistance": "NOT_LONG_DISTANCE",
+          "preferentialService": false,
+          "iconType": "EMPTY"
+        },
+        "promoted": true,
+        "avgRating": "4.2",
+        "totalRatings": 10000,
+        "new": false
+      },
+      "subtype": "basic"
+    }
+  ]
