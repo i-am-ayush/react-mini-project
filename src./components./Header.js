55606d5a2d3a35cc7d151a01516b1cd82c0e7661
i-am-ayush <46191140+i-am-ayush@users.noreply.github.com>
2023-02-01 23:13:02 +0530

    Create Header.js

---
+++ src./components./Header.js
@@ -0,0 +1,21 @@
+export const Title = () => (
+    <a href="/">
+        <img className="logo" alt="logo" src="https://cdn.vox-cdn.com/thumbor/5d_RtADj8ncnVqh-afV3mU-XQv0=/0x0:1600x1067/1200x900/filters:focal(672x406:928x662)/cdn.vox-cdn.com/uploads/chorus_image/image/57698831/51951042270_78ea1e8590_h.7.jpg"></img>
+    </a>
+)
+
+export const Header = () => {
+    return (
+        <div className="header">
+            <Title />
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
