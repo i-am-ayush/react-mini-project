571df1099f627fc7eb6a1bd28d4bcf7df4711bf7
i-am-ayush <46191140+i-am-ayush@users.noreply.github.com>
2023-02-01 23:18:24 +0530

    Add files via upload

---
+++ src/components/RestaurantCard.js
@@ -0,0 +1,19 @@
+import { IMAGE_CDN_URL } from "../constant";
+
+const RestaurantCard = ({
+    name,
+    cuisines,
+    cloudinaryImageId,
+    lastMileTravelString,
+}) => {
+    return (
+        <div className="card">
+            <img src={`${IMAGE_CDN_URL}${cloudinaryImageId}`} />
+            <h2>{name}</h2>
+            <h3>{cuisines.join(", ")}</h3>
+            <h4>{lastMileTravelString}</h4>
+        </div>
+    )
+}
+
+export default RestaurantCard
