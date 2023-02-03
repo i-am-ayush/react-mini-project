787908478a0414923645ef6b21cb5209940e4cd3
i-am-ayush <46191140+i-am-ayush@users.noreply.github.com>
2023-02-03 16:57:19 +0530

    Add files via upload

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,27 +1,43 @@
-import { useState } from "react";
-import { RESTAURANT_LIST } from "../constant";
+// read about shimmer effect, ex-youtube,swiggy
+import { useEffect, useState } from "react";
+// import { RESTAURANT_LIST } from "../constant";
+// we can write only js expresion and statement inside {} not js code
+import Shimmer from "./Shimmer";
 import RestaurantCard from "./RestaurantCard";
-function filterData (searchText, restaurants) {
-    return restaurants.filter((restaurant)=>restaurant.data.name.includes(searchText))
+function filterData(searchText, restaurants) {
+    return restaurants.filter((restaurant) => restaurant?.data?.name?.toLowerCase().includes(searchText.toLowerCase()))
 }
+
 const Body = () => {
-    const searchTextLocal = "somen19"
-    const [searchText, setSearchText] = useState("somen19");
-    let searchClicked = "false";
-    const [searchClick, setSearchClick] = useState("false");
-    const [restaurantList, setRestaurantList] = useState(RESTAURANT_LIST);
-    return (
+    const [searchText, setSearchText] = useState("");
+    const [filteredRestaurantList, setFilteredRestaurantList] = useState([]);
+    const [allRestaurantList, setAllRestaurantList] = useState([]);
+    async function getReastaurantsList() {
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.608087&lng=85.087365&page_type=DESKTOP_WEB_LISTING")
+        const json = await data.json();
+        console.log(json);
+        // optional chaining -> it wontbreak the code if property is not present, it gives undefined
+        setAllRestaurantList(json?.data?.cards[2]?.data?.data?.cards)
+        setFilteredRestaurantList(json?.data?.cards[2]?.data?.data?.cards)
+    }
+    // it will be called once after initial render
+    useEffect(() => {
+        getReastaurantsList();
+        // API call should be done here
+    }, [])
+
+    // useEffect(()=>{
+    //     console.log("render")
+    // }, [restaurantList])
+    // console.log("without use effect render")
+
+    // if (filteredRestaurantList?.length == 0) {
+    //     return <h1>No data matched</h1>
+    // }
+    // conditional rendering
+    return (allRestaurantList.length == 0 ? <Shimmer /> : (
         <>
             <div className="searchContainer">
-                {/* 
-                    not working in react because of one way binding
-                    <input
-                    type="text"
-                    placeholder="Enter search item here"
-                    className="search-input"
-                    value={searchTextLocal}
-                    onChange={(e)=> searchTextLocal = e.target.value}
-                /> */}
                 <input
                     type="text"
                     placeholder="Enter search item here"
@@ -29,32 +45,22 @@ const Body = () => {
                     value={searchText}
                     onChange={(e) => setSearchText(e.target.value)}
                 />
-                {/* <h1>{searchClicked}</h1>
-                <h2>{searchClick}</h2> */}
-                {/* <button onClick={()=>{
-                    searchClicked="false";
-                    setSearchClick("true");
-                }} className="search-btn"> Search - {searchText}</button> */}
                 <button onClick={() => {
-                    const data = filterData(searchText, restaurantList);
-                    setRestaurantList(data);
+                    const data = filterData(searchText, allRestaurantList);
+                    setFilteredRestaurantList(data);
                 }} className="search-btn"> Search </button>
             </div>
-            {/* <div className="restaurantCard">
-                {
-                    RESTAURANT_LIST.map(restaurant => {
-                        return <RestaurantCard {...restaurant.data} key={restaurant.data.id} />
-                    })
-                }
-            </div> */}
-            <div className="restaurantCard">
-                {
-                    restaurantList.map(restaurant => {
-                        return <RestaurantCard {...restaurant.data} key={restaurant.data.id} />
-                    })
-                }
-            </div>
+            {filteredRestaurantList.length == 0 ? <h1>No restaurant data matched</h1> :
+                <div className="restaurantCard">
+                    {
+                        filteredRestaurantList.map(restaurant => {
+                            return <RestaurantCard {...restaurant.data} key={restaurant.data.id} />
+                        })
+                    }
+                </div>
+            }
         </>
+    )
     )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
+import { useState } from "react";
+
 export const Title = () => (
     <a href="/">
         <img className="logo" alt="logo" src="https://cdn.vox-cdn.com/thumbor/5d_RtADj8ncnVqh-afV3mU-XQv0=/0x0:1600x1067/1200x900/filters:focal(672x406:928x662)/cdn.vox-cdn.com/uploads/chorus_image/image/57698831/51951042270_78ea1e8590_h.7.jpg"></img>
     </a>
 )
 
+
 export const Header = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState();
     return (
         <div className="header">
             <Title />
@@ -16,6 +20,7 @@ export const Header = () => {
                     <li>Cart</li>
                 </ul>
             </div>
+            {!isLoggedIn ? <button onClick={()=>setIsLoggedIn(true)}>Login</button> : <button onClick={()=>setIsLoggedIn(false)}>Logout</button>}
         </div>
     )
 }
