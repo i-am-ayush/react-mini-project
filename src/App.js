e250a60c1b68c3ea74db23422c4a69675ef89104
i-am-ayush <46191140+i-am-ayush@users.noreply.github.com>
2023-02-04 11:26:02 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-// Topics to discuss
-// discuss all type of imports
-// 
-import React from "react";
+import React, { Children } from "react";
 import ReactDOM from "react-dom/client";
-// import ways
-// import Header from "./components/Header";
-// import Header from "./components/Header";
-// header.js this also work
-// we can write filename Header.jsx then we can import through Header.jsx also
-import * as obj from "./components/Header.js";
+import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import About from "./components/About";
+import Error from "./components/Error";
+import Contact from "./components/Contact";
+import RestaurantMenu from "./components/RestaurantMenu";
 
 const AppLayout = () => {
   return (
     <>
-      <obj.Header />
-      <Body />
+      <Header />
+      <Outlet/>
       <Footer />
     </>
   )
 }
+
+// This is basic router in which we font find header footer in about page
+// const appRouter = createBrowserRouter([
+//     {
+//         path: "/",
+//         element: <AppLayout/>,
+//         errorElement: <Error/>
+//     },
+//     {
+//         path: "/about",
+//         element: <About/>
+//     },
+
+// ])
+
+const appRouter = createBrowserRouter([
+    {
+        path: "/",
+        element: <AppLayout/>,
+        errorElement: <Error/>,
+        children: [
+            {
+                path: "/",
+                element: <Body/>
+            },
+            {
+                path: "/about",
+                element: <About/>
+            },
+            {
+                path: "/contact",
+                element: <Contact/>
+            },
+            {
+                path: "/restaurant/:id",
+                element: <RestaurantMenu/>
+            },
+
+        ]
+    },
+
+])
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
